a3e316e691b9762b35b9fd03dfb916fca8a4c0d5
unknown <expelmahmud@gmail.com>
2020-04-16 14:54:20 +0600

    .git file fixed

--- polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import polls_list, polls_details
-
-from .apiviews import PollList, PollDetail
-
-urlpatterns = [
-    path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_details")
-]
