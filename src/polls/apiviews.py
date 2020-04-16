48ded4378d9f52933274c1f78b14312e178f1aaa
unknown <expelmahmud@gmail.com>
2020-04-16 15:58:36 +0600

    Generics view added

--- src/polls/apiviews.py
+++ src/polls/apiviews.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
@@ -17,9 +17,20 @@ class PollDetail(generics.RetrieveDestroyAPIView):
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
+        return queryset
     serializer_class = ChoiceSerializer
 
 
 class CreateVote(generics.CreateAPIView):
     serializer_class = VoteSerializer
+
+    def create(self, request, *args, **kwargs):
+        voted_by = request.data.get('voted_by')
+        data = {'choice': kwargs['choice_pk'], 'poll': kwargs['pk'], 'voted_by':voted_by}
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
